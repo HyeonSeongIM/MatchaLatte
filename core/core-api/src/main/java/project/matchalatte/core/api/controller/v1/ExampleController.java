0073b3e9ccfd94c4ec46185e8e40993409efe828
HyeonSeong <gus8131@naver.com>
2025-09-02 19:14:14 +0900

    feat : 예시 api 임의 설정

---
+++ core/core-api/src/main/java/project/matchalatte/core/api/controller/v1/ExampleController.java
@@ -0,0 +1,7 @@
+package project.matchalatte.core.api.controller.v1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+}
---
+++ core/core-api/src/main/java/project/matchalatte/core/api/controller/v1/request/ExampleRequest.java
@@ -0,0 +1,4 @@
+package project.matchalatte.core.api.controller.v1.request;
+
+public record ExampleRequest() {
+}
