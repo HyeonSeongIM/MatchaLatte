cb2a7338e2fb4552592ce9e5058ed8dffb4ec655
HyeonSeong <gus8131@naver.com>
2025-09-02 11:48:21 +0900

    first commit

---
+++ src/main/java/project/matchalatte/MatchaLatteApplication.java
@@ -0,0 +1,13 @@
+package project.matchalatte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchaLatteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchaLatteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/matchalatte/MatchaLatteApplicationTests.java
@@ -0,0 +1,13 @@
+package project.matchalatte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchaLatteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
