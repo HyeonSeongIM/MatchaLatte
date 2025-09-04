aface5292264f68b4ede273cb936a8e985f969d4
HyeonSeong <gus8131@naver.com>
2025-09-04 14:46:06 +0900

    [#2] fix : 인프라 계층 재구성

--- storage/db-core/src/main/java/project/matchalatte/storage/db/core/UserEntity.java
+++ storage/db-core/src/main/java/project/matchalatte/storage/db/core/UserEntity.java
@@ -2,15 +2,11 @@ package project.matchalatte.storage.db.core;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class UserEntity extends BaseEntity {
-
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
 
     @Column(nullable = false)
     private String username;
@@ -18,18 +14,8 @@ public class UserEntity extends BaseEntity {
     public UserEntity() {
     }
 
-    public UserEntity(String email, String password, String username) {
-        this.email = email;
-        this.password = password;
+    public UserEntity(String username) {
         this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public String getUsername() {
