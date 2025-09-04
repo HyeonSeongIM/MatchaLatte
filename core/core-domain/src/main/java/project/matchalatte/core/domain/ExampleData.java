d93f5f58313ee163a43fefa0846e5c02cc2172ed
HyeonSeong <gus8131@naver.com>
2025-09-04 17:42:52 +0900

    [#2] fix : 레거시 삭제

--- core/core-domain/src/main/java/project/matchalatte/core/domain/ExampleData.java
+++
@@ -1,4 +0,0 @@
-package project.matchalatte.core.domain;
-
-public record ExampleData(String value, String param) {
-}
--- storage/db-core/src/main/java/project/matchalatte/storage/db/core/CoreRepository.java
+++
@@ -1,6 +0,0 @@
-package project.matchalatte.storage.db.core;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoreRepository extends JpaRepository<CoreEntity, Long> {
-}
