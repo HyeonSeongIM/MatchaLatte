474b3d496f42c74890c7b5f35f4396eb68e42957
HyeonSeong <gus8131@naver.com>
2025-09-19 13:07:00 +0900

    [#9] feat : 로그인, 유저 읽기 API 테스트 코드 작성

--- core/core-api/src/test/java/project/matchalatte/core/api/controller/v1/UserControllerTest.java
+++ core/core-api/src/test/java/project/matchalatte/core/api/controller/v1/UserControllerTest.java
@@ -18,21 +18,30 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import project.matchalatte.core.api.controller.v1.request.SignInRequest;
 import project.matchalatte.core.api.controller.v1.request.SignUpRequest;
 import project.matchalatte.core.domain.user.User;
 import project.matchalatte.core.domain.user.UserSecurityService;
 import project.matchalatte.core.domain.user.UserService;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Tag("restdocs")
 @WithMockUser
@@ -55,9 +64,9 @@ class UserControllerTest {
     void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
             .apply(documentationConfiguration(restDocumentation)) // ⬅️ 핵심! MockMvc에 REST
-                                                                  // Docs 설정을 적용합니다.
+            // Docs 설정을 적용합니다.
             .addFilters(new CharacterEncodingFilter("UTF-8", true)) // ⬅️ 한글 깨짐 방지를 위한 필터
-                                                                    // 추가
+            // 추가
             .alwaysDo(print()) // ⬅️ 모든 요청/응답을 항상 콘솔에 출력 (디버깅에 유용)
             .build();
     }
@@ -85,6 +94,63 @@ class UserControllerTest {
                     responseFields(fieldWithPath("result").description("API 성공 여부"),
                             fieldWithPath("data.nickname").description("가입된 사용자의 닉네임"),
                             fieldWithPath("error").description("에러 여부"))));
+
+    }
+
+    @Test
+    @DisplayName("로그인 API 테스트")
+    void signInApiTest() throws Exception {
+        // given
+        SignInRequest signInRequest = new SignInRequest("Hyeonseong", "1234");
+        String requestJson = objectMapper.writeValueAsString(signInRequest);
+
+        String mockedToken = "fakeJwtToken";
+
+        given(userSecurityService.signIn(anyString(), anyString())).willReturn(mockedToken);
+
+        // when & then
+        mockMvc.perform(
+                post("/api/v1/user/signIn").with(csrf()).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+            .andExpect(jsonPath("$.data.token").value(mockedToken))
+            .andDo(print())
+            .andDo(document("user-signIn", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                    requestFields(fieldWithPath("username").description("사용자 아이디"),
+                            fieldWithPath("password").description("사용자 비밀번호")),
+                    responseFields(fieldWithPath("result").description("API 성공 여부"),
+                            fieldWithPath("data.token").description("JWT 토큰"),
+                            fieldWithPath("error").description("에러 여부"))
+
+            ));
+    }
+
+    @Test
+    @DisplayName("회원 인증 API 요청 시 헤더에 JWT가 잘 담기는지 확인하는 API")
+    void checkHeaderToJwtTokenApiTest() throws Exception {
+        // given
+        Long mockedUserId = 1L;
+        User mockedUser = new User(1L, "Hyeonseong", "1234", "Dev0");
+
+        String jwtToken = "fakeJwtToken";
+
+        given(userService.read(any())).willReturn(mockedUser);
+
+        // when & then
+        mockMvc
+            .perform(get("/api/v1/user/{id}", mockedUserId).header("Authorization", "Bearer " + jwtToken)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.data.id").value(mockedUserId))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andDo(document("user-auth-check", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                    pathParameters(parameterWithName("id").description("조회할 사용자 ID")),
+                    requestHeaders(headerWithName("Authorization").description("JWT 토큰")),
+                    responseFields(fieldWithPath("result").description("API 성공 여부"),
+                            fieldWithPath("data.id").description("사용자 ID"),
+                            fieldWithPath("data.username").description("사용자 이메일"),
+                            fieldWithPath("error").description("에러 정보 (성공 시 null)"))
+
+            ));
 
     }
 
