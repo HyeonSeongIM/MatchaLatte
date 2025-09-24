d57500790afc7976ec47138830adf2f221a20569
HyeonSeong <gus8131@naver.com>
2025-09-24 10:54:00 +0900

    [#11] feat : UserServiceTest 생성

---
+++ core/core-domain/src/test/java/project/matchalatte/core/domain/user/UserServiceTest.java
@@ -0,0 +1,43 @@
+package project.matchalatte.core.domain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("사용자 저장 확인")
+    void check_save_user() {
+
+    }
+
+    @Test
+    @DisplayName("사용자 읽기 확인")
+    void check_read_user() {
+
+    }
+
+    @Test
+    @DisplayName("사용자 저장 실패 시")
+    void check_save_not_user() {
+
+    }
+
+    @Test
+    @DisplayName("사용자 읽기 시 존재하지 않을 경우")
+    void check_read_not_user() {
+
+    }
+
+}
