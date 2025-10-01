945066d5a5982f58fec44bd44f4691f5f7c92038
HyeonSeong <gus8131@naver.com>
2025-10-01 12:09:14 +0900

    [#24] feat : ProductEntity init

---
+++ storage/db-core/src/main/java/project/matchalatte/storage/db/core/ProductEntity.java
@@ -0,0 +1,43 @@
+package project.matchalatte.storage.db.core;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class ProductEntity extends BaseEntity {
+
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    private String imageUrl;
+
+    public ProductEntity() {
+    }
+
+    public ProductEntity(String name, String description, Long price, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
