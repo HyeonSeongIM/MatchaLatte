4f841943935ad577e036c499a091a34243ffd92a
HyeonSeong <gus8131@naver.com>
2025-10-02 12:16:24 +0900

    [#24] feat : Product 생성 로직 구성

--- core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductService.java
+++ core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductService.java
@@ -4,4 +4,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductCreater productCreater;
+
+    private final ProductReader productReader;
+
+    private final ProductUpdater productUpdater;
+
+    private final ProductDeleter productDeleter;
+
+    public ProductService(ProductCreater productCreater, ProductReader productReader, ProductUpdater productUpdater,
+            ProductDeleter productDeleter) {
+        this.productCreater = productCreater;
+        this.productReader = productReader;
+        this.productUpdater = productUpdater;
+        this.productDeleter = productDeleter;
+    }
+
+    public Product createProduct(String name, String description, Long price) {
+        return productCreater.createProduct(name, description, price);
+    }
+
 }
