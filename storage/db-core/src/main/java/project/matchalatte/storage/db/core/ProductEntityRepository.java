7077ed6bcaf6dfb48b2154aa868c9b0eea10717c
HyeonSeong <gus8131@naver.com>
2025-10-02 12:16:41 +0900

    [#24] feat : JPA 레포와 도메인 레포 연결

--- storage/db-core/src/main/java/project/matchalatte/storage/db/core/ProductEntityRepository.java
+++ storage/db-core/src/main/java/project/matchalatte/storage/db/core/ProductEntityRepository.java
@@ -1,5 +1,52 @@
 package project.matchalatte.storage.db.core;
 
-public class ProductEntityRepository {
+import org.springframework.stereotype.Repository;
+import project.matchalatte.core.domain.product.Product;
+import project.matchalatte.core.domain.product.ProductRepository;
+
+@Repository
+public class ProductEntityRepository implements ProductRepository {
+
+    private final ProductJpaRepository jpaRepository;
+
+    public ProductEntityRepository(ProductJpaRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        ProductEntity productEntity = jpaRepository
+            .save(new ProductEntity(product.name(), product.description(), product.price()));
+
+        return new Product(productEntity.getName(), productEntity.getDescription(), productEntity.getPrice());
+    }
+
+    @Override
+    public Product findById(Long id) {
+        ProductEntity productEntity = jpaRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        return new Product(productEntity.getName(), productEntity.getDescription(), productEntity.getPrice());
+    }
+
+    @Override
+    public Product update(Long id, Product product) {
+        ProductEntity productEntity = jpaRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        productEntity.setName(product.name());
+        productEntity.setDescription(product.description());
+        productEntity.setPrice(product.price());
+
+        return new Product(productEntity.getName(), productEntity.getDescription(), productEntity.getPrice());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ProductEntity productEntity = jpaRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        jpaRepository.delete(productEntity);
+    }
 
 }
