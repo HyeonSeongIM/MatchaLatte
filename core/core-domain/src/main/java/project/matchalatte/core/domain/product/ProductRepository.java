ac7a65443d4d6616b9fe8122f431adee4486baf6
HyeonSeong <gus8131@naver.com>
2025-10-02 13:12:44 +0900

    [#24] feat : JPA 레포와 도메인 레포 연결

--- core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductRepository.java
+++ core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductRepository.java
@@ -1,4 +1,13 @@
 package project.matchalatte.core.domain.product;
 
 public interface ProductRepository {
+
+    Product save(Product product);
+
+    Product findById(Long id);
+
+    Product update(Long id, Product product);
+
+    void deleteById(Long id);
+
 }
