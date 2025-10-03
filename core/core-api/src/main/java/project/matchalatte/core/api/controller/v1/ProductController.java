d6ee2d89c0b04641533fee0c7c25753c81d9b308
HyeonSeong <gus8131@naver.com>
2025-10-03 14:33:54 +0900

    [#24] feat : 상품 업데이트 API 생성

--- core/core-api/src/main/java/project/matchalatte/core/api/controller/v1/ProductController.java
+++ core/core-api/src/main/java/project/matchalatte/core/api/controller/v1/ProductController.java
@@ -2,8 +2,10 @@ package project.matchalatte.core.api.controller.v1;
 
 import org.springframework.web.bind.annotation.*;
 import project.matchalatte.core.api.controller.v1.request.ProductCreateRequest;
+import project.matchalatte.core.api.controller.v1.request.ProductUpdateRequest;
 import project.matchalatte.core.api.controller.v1.response.ProductCreateResponse;
 import project.matchalatte.core.api.controller.v1.response.ProductReadResponse;
+import project.matchalatte.core.api.controller.v1.response.ProductUpdateResponse;
 import project.matchalatte.core.domain.product.Product;
 import project.matchalatte.core.domain.product.ProductService;
 import project.matchalatte.core.support.response.ApiResponse;
@@ -28,6 +30,13 @@ public class ProductController {
     public ApiResponse<ProductReadResponse> readProductById(@PathVariable("id") Long id) {
         Product result = productService.readProductById(id);
         return ApiResponse.success(new ProductReadResponse(result.name(), result.description(), result.price()));
+    }
+
+    @PutMapping("/{id}")
+    public ApiResponse<ProductUpdateResponse> updateProduct(@PathVariable("id") Long id,
+            @RequestBody ProductUpdateRequest request) {
+        Product result = productService.updateProduct(id, request.name(), request.description(), request.price());
+        return ApiResponse.success(new ProductUpdateResponse(result.name(), result.description(), result.price()));
     }
 
 }
--- core/core-api/src/test/java/project/matchalatte/core/api/controller/v1/ProductControllerTest.java
+++ core/core-api/src/test/java/project/matchalatte/core/api/controller/v1/ProductControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import project.matchalatte.core.api.controller.v1.request.ProductCreateRequest;
+import project.matchalatte.core.api.controller.v1.request.ProductUpdateRequest;
 import project.matchalatte.core.domain.product.Product;
 import project.matchalatte.core.domain.product.ProductService;
 
@@ -19,8 +20,7 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -86,6 +86,33 @@ class ProductControllerTest {
             .andExpect(jsonPath("$.data.name").value("아이돌 굿즈"))
             .andExpect(jsonPath("$.data.description").value("BTS 정국 굿즈에요"))
             .andExpect(jsonPath("$.data.price").value(50000L))
+            .andExpect(jsonPath("$.error").value(nullValue()));
+    }
+
+    @Test
+    @DisplayName("상품 업데이트 API 정상 테스트")
+    void productUpdate_API() throws Exception {
+        // given
+        Long productId = 1L;
+
+        ProductUpdateRequest updateRequest = new ProductUpdateRequest("아이패드", "사용한지 1개월 정도 됐어요.", 60000L);
+        Product updatedProduct = new Product(updateRequest.name(), updateRequest.description(), updateRequest.price());
+
+        given(productService.updateProduct(productId, updateRequest.name(), updateRequest.description(),
+                updateRequest.price()))
+            .willReturn(updatedProduct);
+
+        // when & then
+        mockMvc
+            .perform(put("/api/v1/product/" + productId).contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateRequest))
+                .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value("SUCCESS"))
+            .andExpect(jsonPath("$.data.name").value("아이패드"))
+            .andExpect(jsonPath("$.data.description").value("사용한지 1개월 정도 됐어요."))
+            .andExpect(jsonPath("$.data.price").value(60000L))
             .andExpect(jsonPath("$.error").value(nullValue()));
     }
 
