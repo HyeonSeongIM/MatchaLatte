89f34e3286ead10c43296aeddf43783b7fbeb48f
HyeonSeong <gus8131@naver.com>
2025-10-08 12:19:41 +0900

    [#26] refactor : 서비스 계층에서 책임을 없애고 구현 계층에서 책임지기 - 서비스 계층에서는 어떻게 비즈니스 로직이 수행되는지 알 필요가 없어짐. - 테스트 코드 작성 시에도 역할과 책임을 잘 분리할 수 있음.

--- core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductDeleter.java
+++ core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductDeleter.java
@@ -7,12 +7,20 @@ public class ProductDeleter {
 
     private final ProductRepository productRepository;
 
-    public ProductDeleter(ProductRepository productRepository) {
+    private final ProductValidator productValidator;
+
+    public ProductDeleter(ProductRepository productRepository, ProductValidator productValidator) {
         this.productRepository = productRepository;
+        this.productValidator = productValidator;
     }
 
-    public void deleteById(Long id) {
-        productRepository.deleteById(id);
+    public void deleteById(Long productId, Long userId) {
+        if (productValidator.matchUserById(userId, productId)) {
+            productRepository.deleteById(productId);
+        }
+        else {
+            throw new IllegalArgumentException("User does not have permission for this product.");
+        }
     }
 
 }
--- core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductService.java
+++ core/core-domain/src/main/java/project/matchalatte/core/domain/product/ProductService.java
@@ -13,15 +13,12 @@ public class ProductService {
 
     private final ProductDeleter productDeleter;
 
-    private final ProductValidator productValidator;
-
     public ProductService(ProductCreater productCreater, ProductReader productReader, ProductUpdater productUpdater,
-            ProductDeleter productDeleter, ProductValidator productValidator) {
+            ProductDeleter productDeleter) {
         this.productCreater = productCreater;
         this.productReader = productReader;
         this.productUpdater = productUpdater;
         this.productDeleter = productDeleter;
-        this.productValidator = productValidator;
     }
 
     public Product createProduct(String name, String description, Long price, Long userId) {
@@ -33,21 +30,11 @@ public class ProductService {
     }
 
     public Product updateProduct(Long productId, String name, String description, Long price, Long userId) {
-        if (productValidator.matchUserById(productId, userId)) {
-            return productUpdater.updateProduct(productId, name, description, price, userId);
-        }
-        else {
-            throw new IllegalArgumentException("Product with id " + productId + " does not exist");
-        }
+        return productUpdater.updateProduct(productId, name, description, price, userId);
     }
 
     public void deleteProductById(Long productId, Long userId) {
-        if (productValidator.matchUserById(productId, userId)) {
-            productDeleter.deleteById(productId);
-        }
-        else {
-            throw new IllegalArgumentException("Product with id " + productId + " does not exist");
-        }
+        productDeleter.deleteById(productId, userId);
     }
 
 }
