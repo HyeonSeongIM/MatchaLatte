dd55722d16d231c98dec9575e2ffab2a43847696
HyeonSeong <gus8131@naver.com>
2025-10-10 12:24:12 +0900

    [#28] fix : Entity와 Jpa레포 스캔 코드 추가

--- storage/db-core/src/test/java/project/matchalatte/core/CoreDbContextTest.java
+++ storage/db-core/src/test/java/project/matchalatte/core/CoreDbContextTest.java
@@ -2,14 +2,19 @@ package project.matchalatte.core;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 
 @ActiveProfiles("local")
 @Tag("context")
-@SpringBootTest
+@DataJpaTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@EntityScan(basePackages = "project.matchalatte.storage.db.core")
+@EnableJpaRepositories(basePackages = "project.matchalatte.storage.db.core")
 public abstract class CoreDbContextTest {
 
     @Test
