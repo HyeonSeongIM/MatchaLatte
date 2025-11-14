164020a5d4fce7fe8004c1da5d209bf6b3a5d8c9
HyeonSeong <gus8131@naver.com>
2025-11-14 12:40:30 +0900

    [#44] feat : 관리자 대시보드 표출 정보 DTO

---
+++ admin/src/main/java/project/matchalatte/admin/domain/PriceRange.java
@@ -0,0 +1,6 @@
+package project.matchalatte.admin.domain;
+
+public record PriceRange(String priceRange, Long totalCount
+
+) {
+}
---
+++ admin/src/main/java/project/matchalatte/admin/domain/TopSeller.java
@@ -0,0 +1,4 @@
+package project.matchalatte.admin.domain;
+
+public record TopSeller(String nickname, Long productCount, Long totalPrice) {
+}
