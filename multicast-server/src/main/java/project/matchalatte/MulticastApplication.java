a75a7233adf5655d26ec8c3fbe24850777ed117e
HyeonSeong <gus8131@naver.com>
2025-11-21 15:22:59 +0900

    [#42] feat : 전송 서버 생성

---
+++ multicast-server/src/main/java/project/matchalatte/MulticastApplication.java
@@ -0,0 +1,15 @@
+package project.matchalatte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class MulticastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulticastApplication.class, args);
+    }
+
+}
