d3246a7a90b0e83ea6e624ff59107a70f7a8366e
HyeonSeong <gus8131@naver.com>
2026-01-09 16:30:16 +0900

    hotFix : 서버 간 통신 Config 파일 생성

---
+++ admin/src/main/java/project/matchalatte/admin/api/config/HttpClientConfig.java
@@ -0,0 +1,20 @@
+package project.matchalatte.admin.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(3))
+            .followRedirects(HttpClient.Redirect.NORMAL)
+            .build();
+    }
+
+}
